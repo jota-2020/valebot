a899f12952f04ba958e3745987d037dc1641d13d
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-12 02:58:56 -0400

    Update mipilot-serbot-broadcast.js

--- plugins/mipilot-serbot-broadcast.js
+++ plugins/mipilot-serbot-broadcast.js
@@ -1,4 +1,4 @@
-let handler = async (m, { conn, usedPrefix, text }) => {
+/*let handler = async (m, { conn, usedPrefix, text }) => {
     if (conn.user.jid !== global.conn.user.jid) throw false
     let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user.jid)])]
     let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
