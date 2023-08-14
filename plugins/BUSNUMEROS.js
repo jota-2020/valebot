ba9c69672940be6afd8923e8239f0336092c7538
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 11:04:29 -0400

    Update BUSNUMEROS.js

--- plugins/BUSNUMEROS.js
+++ plugins/BUSNUMEROS.js
@@ -1,12 +1,7 @@
 let handler = async (m, { conn, text}) => {
 
-m.reply(`*╔══════════*\n➢ *"${pickRandom(global.numeros)}"*\n*╚═══════════════*`)
+m.reply(`Call center 800 370 030, Trafico General: wa.me/56995355239   Monitoreo: wa.me/56957592341`)
 }
 handler.tags = ['frases']
 handler.command = ['numeros|num|ayuda']
 export default handler
-
-function pickRandom(list) {
-return list[Math.floor(list.length * Math.random())]}
-
-global.numeros = ["Call center 800 370 030, Trafico General: wa.me/56995355239   Monitoreo: wa.me/56957592341"]
