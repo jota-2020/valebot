65c61475fbff09312b72c0fb647d5830145054d6
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 11:14:46 -0400

    Update BUSNUMEROS.js

--- plugins/BUSNUMEROS.js
+++ plugins/BUSNUMEROS.js
@@ -1,8 +1,9 @@
-let handler = async (m, { conn }) => {
-if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = 'Call center 800 370 030, Trafico General: wa.me/56995355239   Monitoreo: wa.me/56957592341'
-conn.sendPresenceUpdate('recording', m.chat)
-conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3` }, { quoted: m })}
-handler.customPrefix = /ª|a|A/
-handler.command = /^(numeros|num|ayuda?$)/
+async function handler(m, { usedPrefix }) {
+let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]  
+const message = users.map(v => '👉 wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}estado\n(${v.name})\n\n`).join('\n')
+const replyMessage = (message.length === 0) ? "*Call center 800 370 030*" : message
+await m.reply( '*Call center 800 370 030, Trafico General: wa.me/56995355239   Monitoreo: wa.me/56957592341*')
+await m.reply(replyMessage.trim())}
+handler.command = handler.help = ['num','numeros','ayuda']
+handler.tags = ['numero']
 export default handler
