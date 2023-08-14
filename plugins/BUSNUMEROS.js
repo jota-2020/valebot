419ef45f9dc956038f56d6e4b776917ab99e53b5
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 15:24:06 -0400

    Update BUSNUMEROS.js

--- plugins/BUSNUMEROS.js
+++ plugins/BUSNUMEROS.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text}) => {
 
-m.reply(`*╔═══════════════════════════*\n➢ *"${pickRandom(global.piropo)}"*\n*╚═══════════════════════════*`)
+m.reply(`*╔═══════════════════════════*\n➢ *"${pickRandom(global.numeros)}"*\n*╚═══════════════════════════*`)
 }
 handler.tags = ['frases']
 handler.command = ['numeros','num','numero','trafico','tráfico','monitoreo']
@@ -9,4 +9,4 @@ export default handler
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]}
 
-global.piropo = ["CALL CENTER 800 370 030 \n➢TRAFICO: wa.me/56995355239\n➢MONITOREO: wa.me/56957592341"]
+global.numeros = ["CALL CENTER 800 370 030 \n➢TRAFICO: wa.me/56995355239\n➢MONITOREO: wa.me/56957592341"]
