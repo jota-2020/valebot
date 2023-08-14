48dbe5e473b260b147ab202ba3fd0d732b0aee46
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 15:34:58 -0400

    Update buk.js

--- plugins/buk.js
+++ plugins/buk.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 let handler = async (m, { conn, command, args }) => {
 const fetch = (await import('node-fetch')).default
 let text = args.join` `
-if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 NUMERO DE BUS TURBUS QUE DESEE BUSCAR*', m)
+//if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 NUMERO DE BUS TURBUS QUE DESEE BUSCAR*', m)
 let url = 'https://gtppersonas.buk.cl/users/sign_in#liquidaciones/188818'
-let search = await googleIt(text)
+//let search = await googleIt(text)
 let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
 try {
 //let ss = `https://image.thum.io/get/width/1200/height/900/crop/800/${url}`
