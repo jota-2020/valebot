76d948db6b329e85beb1878b0d247c0f416317fc
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 14:34:58 -0400

    Update BUSNUMEROS.js

--- plugins/BUSNUMEROS.js
+++ plugins/BUSNUMEROS.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, text}) => {
 
 m.reply(`*╔═══════════════════════════*\n➢ *"${pickRandom(global.piropo)}"*\n*╚═══════════════════════════*`)
+  m.reply(`*╔═══════════════════════════*\n➢ *ALL CENTER 800 370 030➢TRAFICO: wa.me/56995355239➢MONITOREO: wa.me/56957592341*╚═══════════════════════════*`)
 }
 handler.tags = ['frases']
 handler.command = ['numeros']
