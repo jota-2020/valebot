ae70418723d5bdad7c1c9e22b76998098f95a89e
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 10:27:03 -0400

    Update bus.js

--- plugins/bus.js
+++ plugins/bus.js
@@ -15,33 +15,6 @@ m.reply(msg)
 }}
 handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
 handler.tags = ['internet']
-handler.command = /^googlef?$/i
+handler.command = /^bus|turbus?$/i
 export default handler
 
-/*let ss2 = await ssweb(url, 'desktop')
-let dataa = ss2.result
-async function ssweb(url, device = 'desktop'){
-return new Promise((resolve, reject) => {
-const base = 'https://www.screenshotmachine.com'
-const param = { url: url, device: device, cacheLimit: 0 }
-axios({url: base + '/capture.php', method: 'POST', data: new URLSearchParams(Object.entries(param)), headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }}).then((data) => {
-const cookies = data.headers['set-cookie']
-if (data.data.status == 'success') {
-axios.get(base + '/' + data.data.link, { headers: { 'cookie': cookies.join('') }, responseType: 'arraybuffer' }).then(({ data }) => {
-let result = { status: 200, author: '@BrunoSobrino', result: data } 
-resolve(result)})
-} else {
-reject({ status: 404, author: 'Ryzn', message: data.data })}}).catch(reject)})}*/
-var notFond = {
-status: link.status,
-Pesan: eror}
-return notFond}}
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗️𝐈𝐍𝐅𝐎❗️] 𝙴𝚂𝚃𝙰𝚂 𝚄𝚂𝙰𝙽𝙳𝙾 𝙼𝙰𝙻 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾!!*\n*𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾:*\n*${usedPrefix + command} 𝙿𝚊𝚕𝚊𝚋𝚛𝚊 𝚌𝚕𝚊𝚟𝚎 𝚊 𝚋𝚞𝚜𝚌𝚊𝚛*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Estrellas*`
-wikipedia(`${text}`).then(res => {
-m.reply(`*𝙰𝚀𝚄𝙸 𝚃𝙸𝙴𝙽𝙴𝚂 𝙻𝙰 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙰:*\n\n` + res.result.isi)
-}).catch(() => { m.reply('*[❗️𝐈𝐍𝐅𝐎❗️] 𝙽𝙾 𝚂𝙴 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙾 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽, 𝙿𝚁𝚄𝙴𝙱𝙰 𝚀𝚄𝙴 𝙷𝙰𝚈𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝚃𝙾 𝚄𝙽𝙰 𝚂𝙾𝙻𝙰 𝙿𝙰𝙻𝙰𝙱𝚁𝙰 𝚈 𝙻𝙾 𝙷𝙰𝚈𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝚃𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴*') })}
-handler.help = ['wikipedia'].map(v => v + ' <apa>')
-handler.tags = [ 'internet']
-handler.command = /^(bus|turbus)$/i
-export default handler
