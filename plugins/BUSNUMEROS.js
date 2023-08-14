c6136e34661f208faa69e282b77e9075cf939ce5
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 11:10:00 -0400

    Update BUSNUMEROS.js

--- plugins/BUSNUMEROS.js
+++ plugins/BUSNUMEROS.js
@@ -1,7 +1,8 @@
-let handler = async (m, { conn, text}) => {
-
-m.reply(`Call center 800 370 030, Trafico General: wa.me/56995355239   Monitoreo: wa.me/56957592341`)
-}
-handler.tags = ['frases']
-handler.command = ['numeros|num|ayuda']
-
+let handler = async (m, { conn }) => {
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
+let vn = 'Call center 800 370 030, Trafico General: wa.me/56995355239   Monitoreo: wa.me/56957592341'
+conn.sendPresenceUpdate('recording', m.chat)
+conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `a.mp3` }, { quoted: m })}
+handler.customPrefix = /ª|a|A/
+handler.command = /^(numeros|num|ayuda?$)/
+export default handler
