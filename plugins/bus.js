0c8fbbaf12c03ce9055633e24075c5cb3b85d5eb
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-14 10:25:41 -0400

    Update bus.js

--- plugins/bus.js
+++ plugins/bus.js
@@ -1,25 +1,37 @@
-import axios from "axios"
-import fetch from "node-fetch"
-import cheerio from "cheerio"
-async function wikipedia(querry) {
+import { googleIt } from '@bochilteam/scraper'
+import axios from 'axios' 
+let handler = async (m, { conn, command, args }) => {
+const fetch = (await import('node-fetch')).default
+let text = args.join` `
+if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m)
+let url = 'https://busqueda.turbus.cl/Turbusqueda-war/centerzone.jsp?zone=3&idbus=' + encodeURIComponent(text)
+let search = await googleIt(text)
+let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
 try {
-const link = await axios.get(`https://busqueda.turbus.cl/Turbusqueda-war/centerzone.jsp?zone=3&idbus=${querry}`)
-const $ = cheerio.load(link.data)
-let judul = $('#firstHeading').text().trim()
-let thumb = $('#mw-content-text').find('div.mw-parser-output > div:nth-child(1) > table > tbody > tr:nth-child(2) > td > a > img').attr('src') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`
-let isi = []
-$('#mw-content-text > div.mw-parser-output').each(function (rayy, Ra) {
-let penjelasan = $(Ra).find('p').text().trim() 
-isi.push(penjelasan)})
-for (let i of isi) {
-const data = {
-status: link.status,
-result: {
-judul: judul,
-thumb: 'https:' + thumb,
-isi: i}}
-return data}
-} catch (err) {
+let ss = `https://image.thum.io/get/fullpage/${url}`
+await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
+} catch {
+m.reply(msg)
+}}
+handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.tags = ['internet']
+handler.command = /^googlef?$/i
+export default handler
+
+/*let ss2 = await ssweb(url, 'desktop')
+let dataa = ss2.result
+async function ssweb(url, device = 'desktop'){
+return new Promise((resolve, reject) => {
+const base = 'https://www.screenshotmachine.com'
+const param = { url: url, device: device, cacheLimit: 0 }
+axios({url: base + '/capture.php', method: 'POST', data: new URLSearchParams(Object.entries(param)), headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }}).then((data) => {
+const cookies = data.headers['set-cookie']
+if (data.data.status == 'success') {
+axios.get(base + '/' + data.data.link, { headers: { 'cookie': cookies.join('') }, responseType: 'arraybuffer' }).then(({ data }) => {
+let result = { status: 200, author: '@BrunoSobrino', result: data } 
+resolve(result)})
+} else {
+reject({ status: 404, author: 'Ryzn', message: data.data })}}).catch(reject)})}*/
 var notFond = {
 status: link.status,
 Pesan: eror}
