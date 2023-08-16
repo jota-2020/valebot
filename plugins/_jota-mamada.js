a2dd99dae5c7576fbd233e7a76e97261dc10d591
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-16 11:48:35 -0400

    Update and rename mamada.js to _jota-mamada.js

--- plugins/_jota-mamada.js
+++ plugins/_jota-mamada.js
@@ -5,7 +5,7 @@ let a = ps.getRandom()
 let b
 do b = ps.getRandom()
 while (b === a)
-m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 HACERLE UNA MAMADA A ${toM(b)}, DE SEGURO TE LO AGRADECERÁ 💓*`, null, {
+m.reply(`*${toM(a)}, DEBERIAS DARLE UNA MAMADA A ${toM(b)}, DE SEGURO TE LO AGRADECERÁ 😏🥵*`, null, {
 mentions: [a, b]
 })}
 handler.help = ['formarpareja']
@@ -13,3 +13,4 @@ handler.tags = ['main', 'fun']
 handler.command = ['darmamada','mamadas']
 handler.group = true
 export default handler
+
