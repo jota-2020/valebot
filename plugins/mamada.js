4b0deee35e78334dfb8bd99447c10c50d318a75b
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-16 11:38:23 -0400

    Create mamada.js

---
+++ plugins/mamada.js
@@ -0,0 +1,15 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)}, 𝙳𝙴𝙱𝙴𝚁𝙸𝙰𝚂 HACERLE UNA MAMADA A ${toM(b)}, DE SEGURO TE LO AGRADECERÁ 💓*`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['darmamada','mamadas']
+handler.group = true
+export default handler
