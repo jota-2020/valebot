1cf436035601c3ec212adb49aa70383da9b87e93
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-17 17:30:19 -0400

    Update _jota-evita.js

--- plugins/_jota-evita.js
+++ plugins/_jota-evita.js
@@ -2,11 +2,6 @@ import { sticker } from '../lib/sticker.js'
 let handler = m => m
 handler.all = async function (m, { conn }) {
 let chat = global.db.data.chats[m.chat]
-  
-if (chat.audios && m.text.match(/(eva|evi|darkness)/gi)) {    
-let vn = './st/emo3.webp'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})} 
 
 let toM = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata }) {
@@ -18,6 +13,12 @@ while (b === a)
 m.reply(`*${toM(a)}, LE DIO UN BESO A EVITA PROVOCANDO QUE SE MOJE🥵😏🥵*`, null, {
 mentions: [a, b]
 })}
+
+if (chat.audios && m.text.match(/(eva|evi|darkness|EVI|EVITA)/gi)) {    
+let vn = './st/emo3.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})} 
+  
 handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
 handler.command = ['evita','eva','evi','darkness']
