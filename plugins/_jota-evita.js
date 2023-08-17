492906ca64f8faed9382dbd2d43d89b05bc6eb2a
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-17 18:27:45 -0400

    Update _jota-evita.js

--- plugins/_jota-evita.js
+++ plugins/_jota-evita.js
@@ -1,6 +1,6 @@
 
 
-/*let toM = a => '@' + a.split('@')[0]
+let toM = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata }) {
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
@@ -15,4 +15,4 @@ handler.tags = ['main', 'fun']
 handler.command = ['evita','eva','evi','darkness']
 handler.group = true
 export default handler
-*/
+
