80fab5a1dd64492e022bd303703927fe9cab5620
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-17 18:26:53 -0400

    Update adult-comandos+18.js

--- plugins/adult-comandos+18.js
+++ plugins/adult-comandos+18.js
@@ -146,7 +146,6 @@ let res = await conn.getFile(`https://api-fgmods.ddns.net/api/nsfw/lesbian?apike
 if (res == '' || !res || res == null) res = await resError[Math.floor(resError.length * Math.random())]  
 conn.sendMessage(m.chat, { image: { url: res }, caption: `_${command}_`.trim() }, { quoted: m })}  
   
-}
 handler.help = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglss', 'nsfworgy', 'yuri', 'yuri2', 'yaoi', 'yaoi2', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos']
 handler.command = ['nsfwloli', 'nsfwfoot', 'nsfwass', 'nsfwbdsm', 'nsfwcum', 'nsfwero', 'nsfwfemdom', 'nsfwfoot', 'nsfwglss', 'nsfworgy', 'yuri', 'yuri2', 'yaoi', 'yaoi2', 'panties', 'tetas', 'booty', 'ecchi', 'furro', 'hentai', 'trapito', 'imagenlesbians', 'pene', 'porno', 'randomxxx', 'pechos']
 handler.tags = ['nsfw']
