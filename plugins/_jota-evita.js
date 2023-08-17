a1f1d758757e50cfbf0ef524ae1ea774ac3c50e9
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-17 17:13:10 -0400

    Update _jota-evita.js

--- plugins/_jota-evita.js
+++ plugins/_jota-evita.js
@@ -1,18 +1,15 @@
 let toM = a => '@' + a.split('@')[0]
-let toM2 = a => '@56932025033'[0]
 function handler(m, { groupMetadata }) {
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
 let b
 do b = ps.getRandom()
 while (b === a)
-m.reply(`*${toM(a)}, DEBERIAS DARLE UNA MAMADA A ${toM2(b)}, DE SEGURO TE LO AGRADECERÁ 😏🥵  *`, null, {
-mentions: [a, b]
-m.reply(`#kiss @56932025033`, null, {
+m.reply(`*${toM(a)}, DEBERIAS DARLE UNA MAMADA A ${toM(b)}, DE SEGURO TE LO AGRADECERÁ 😏🥵*`, null, {
 mentions: [a, b]
 })}
 handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
-handler.command = ['evita','eva']
+handler.command = ['darmamada','mamadas']
 handler.group = true
 export default handler
