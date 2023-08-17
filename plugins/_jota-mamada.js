7d90420956de6b816c738373b774c356f830ae04
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-08-17 17:19:41 -0400

    Update _jota-mamada.js

--- plugins/_jota-mamada.js
+++ plugins/_jota-mamada.js
@@ -2,7 +2,11 @@ import { sticker } from '../lib/sticker.js'
 let handler = m => m
 handler.all = async function (m, { conn }) {
 let chat = global.db.data.chats[m.chat]
-
+    
+if (chat.audios && m.text.match(/(evi|eva|evita|darness)/gi)) {    
+let vn = './st/emo3.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})} 
 
 let toM = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata }) {
@@ -14,12 +18,6 @@ while (b === a)
 m.reply(`*${toM(a)}, LE DIO UN BESO A EVITA PROVOCANDO QUE SE MOJE🥵*`, null, {
 mentions: [a, b]
 })}
-
-if (chat.audios && m.text.match(/(evi|eva|evita|darness)/gi)) {    
-let vn = './st/emo3.webp'
-this.sendPresenceUpdate('recording', m.chat)   
-this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})} 
-    
 handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
 handler.command = ['darmamada','mamadas']
