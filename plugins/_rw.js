fc1a930c4bf52880137c4c49e22179896b5097c4
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-09-12 09:01:47 -0300

    Create _rw.js

---
+++ plugins/_rw.js
@@ -0,0 +1,15 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`#rw`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['rw','rollwaifu']
+handler.group = true
+export default handler
