1877ed09862af65be5aa98f015613af9ea456434
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-09-12 15:17:31 -0300

    Update _rw.js

--- plugins/_rw.js
+++ plugins/_rw.js
@@ -1,11 +1,12 @@
 let toM = a => '@' + a.split('@')[0]
-let user = global.db.data.users[m.sender]
 function handler(m, { groupMetadata }) {
 let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.${taguser}
-m.reply(`*Suerte!!*`)}
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`Suerte!!`)}
 handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
 handler.command = ['rw','rollwaifu']
 handler.group = true
-export default handler
