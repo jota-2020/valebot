46fc2c7b68e482490f9a7861fc4ddf8d4c91b747
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-09-16 18:18:07 -0300

    Update iptv.js

--- plugins/iptv.js
+++ plugins/iptv.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/cl.m3u'
+let vn = './media/cl.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
-conn.sendFile(m.chat, { audio: { url: vn }, ptt: true, mimetype: 'audio/mpeg', fileName: `cl.m3u` }, { quoted: m })}
+conn.sendMessage(m.chat, { audio: { url: vn }, ptt: true, mimetype: 'audio/mpeg', fileName: `cl.m3u` }, { quoted: m })}
 handler.customPrefix = /Iptv|iptv/
 handler.command = /^(Iptv|iptv?$)/
 export default handler
