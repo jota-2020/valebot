c964fd6bbc46727516b78d027e32df347060fede
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-09-16 18:05:03 -0300

    Update iptv.js

--- plugins/iptv.js
+++ plugins/iptv.js
@@ -3,7 +3,7 @@ let handler = m => m
 handler.all = async function (m, { conn }) {
 let chat = global.db.data.chats[m.chat]
 
-if (chat.audios && m.text.match(/(iptv)/gi)) {    
+if (chat.audios && m.text.match(/(iptv|listaiptv|tv|cable)/gi)) {    
 let vn = './src/cl.m3u
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, { document: { url: dl_url }, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m})
