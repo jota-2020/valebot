33e03bc9fc08bf25a03c647a16d56ea70c23a2b3
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-09-16 22:06:41 -0300

    Update sticker-sticker2.js

--- plugins/sticker-sticker2.js
+++ plugins/sticker-sticker2.js
@@ -14,8 +14,8 @@ let img = await q.download?.()
 if (!img) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰 𝚄𝙽 𝚅𝙸𝙳𝙴𝙾, 𝙸𝙼𝙰𝙶𝙴𝙽 𝙾 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙲𝙸𝙾́𝙽 .𝚓𝚙𝚐 𝙴𝙻 𝙲𝚄𝙰𝙻 𝚂𝙴𝚁𝙰 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙸𝙳𝙾 𝙴𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁, 𝙳𝙴𝙱𝙴 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴𝚁 𝙾 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 ${usedPrefix + command}*`
 let out
 try {
-stiker = await sticker(img, false, global.packname, global.author)
-  await conn.sendFile(m.chat, out, 'error.png', null, m)
+stiker = await conn.sendFile(m.chat, out, 'error.png', null, m)
+  
 } catch (e) {
 console.error(e)
 } finally {
