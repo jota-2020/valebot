8fd0f5ec8b4744b70ac371a8d0c1dca7106c4244
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-10-10 14:05:26 -0300

    Update nv-global.js

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -1111,4 +1111,4 @@ this.sendPresenceUpdate('recording', m.chat)
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})} 
     
 return !0 }
-export default handler*/
+export default handler
