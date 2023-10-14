bfdc50311af6e61c841b549af52e636d64f5dab3
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-10-13 23:30:48 -0300

    Update _jota_blanco.js

--- plugins/_jota_blanco.js
+++ plugins/_jota_blanco.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn, text, command }) => {
-let yh = global.loli
+let yh = global.blanco
 let url = yh[Math.floor(Math.random() * yh.length)]
 conn.sendMessage(m.chat, { image: { url: url }, caption: "white_vale" }, { quoted: m });
 }
@@ -8,7 +8,7 @@ handler.tags = ['anime']
 handler.help = ['loli']
 export default handler
 
-global.loli = [
+global.blanco = [
 "https://telegra.ph/file/9720e672d4013b367dd9f.jpg",
 "https://telegra.ph/file/a082dc8c30501c7d131ce.jpg",
 "https://telegra.ph/file/f0da437b40b3172dddc1e.jpg",
