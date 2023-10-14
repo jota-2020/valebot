7a1a9f0a3236fc90243b532ec8fddc1771119ed5
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-10-13 23:31:14 -0300

    Update _jota_negro.js

--- plugins/_jota_negro.js
+++ plugins/_jota_negro.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn, text, command }) => {
-let yh = global.loli
+let yh = global.negro
 let url = yh[Math.floor(Math.random() * yh.length)]
 conn.sendMessage(m.chat, { image: { url: url }, caption: "Dark_vale" }, { quoted: m });
 }
@@ -8,7 +8,7 @@ handler.tags = ['anime']
 handler.help = ['loli']
 export default handler
 
-global.loli = [
+global.negro = [
 "https://telegra.ph/file/553e40b028cb4a63c64c6.jpg",
 "https://telegra.ph/file/e3350a4c61119a6a62519.jpg",
 "https://telegra.ph/file/3fe53f57440ac1385b5ca.jpg",
