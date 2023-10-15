30a1d51d8f550c6c0731d83731291440983a01cf
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-10-14 23:57:30 -0300

    Update _jota_ship.js

--- plugins/_jota_ship.js
+++ plugins/_jota_ship.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, command, text }) => {
 let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
-*El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
-*Deberias pedirle que sea tu  novia/o ?*
+*El amor de ${text} por ${text} es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
+
 `.trim()
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['love']
