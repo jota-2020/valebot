73be6005d4eb0219613df8a70ada91d62230f3d8
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-10-14 23:56:03 -0300

    Update _jota_ship.js

--- plugins/_jota_ship.js
+++ plugins/_jota_ship.js
@@ -1,10 +1,4 @@
-import MessageType from '@adiwajshing/baileys'
 let handler = async (m, { conn, command, text }) => {
-let who
-if (m.isGroup) who = m.mentionedJid[0]
-else who = m.chat
-if (!who) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰 𝙰 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙲𝙾𝙽 𝙴𝙻 @𝚝𝚊𝚐*'
-let txt = text.replace('@' + who.split`@`[0], '').trim()
 let love = `*❤️❤️ MEDIDOR DE AMOR ❤️❤️*
 *El amor de ${text} por ti es de* *${Math.floor(Math.random() * 100)}%* *de un 100%*
 *Deberias pedirle que sea tu  novia/o ?*
