11ac8175ba22e68e7143c234d7de6bf17e400844
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-10-15 17:13:40 -0300

    Create _jota_shipst.js

---
+++ plugins/_jota_shipst.js
@@ -0,0 +1,9 @@
+import { sticker } from '../lib/sticker.js'
+let handler = m => m
+handler.all = async function (m, { conn }) {
+let chat = global.db.data.chats[m.chat]
+
+if (chat.audios && m.text.match(/(#ship)/gi)) {    
+let vn = './st/1_love.webp'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
