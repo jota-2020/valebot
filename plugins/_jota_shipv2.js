5c18b18e6f9d9d039b50efb001affaa4b801fde0
jota-2020 <70615677+jota-2020@users.noreply.github.com>
2023-10-15 16:42:50 -0300

    Update _jota_shipv2.js

--- plugins/_jota_shipv2.js
+++ plugins/_jota_shipv2.js
@@ -8,7 +8,7 @@ let love = `*Shipeados*
 ===========================
 `.trim()
 let url = yh[Math.floor(Math.random() * yh.length)]
-conn.sendMessage(m.chat, { image: { url: url }, caption = love}, { quoted: m });
+conn.sendMessage(m.chat, { image: { url: url }, caption=love,} { quoted: m });
 }
 handler.command = /^(shop)$/i
 handler.tags = ['anime']
@@ -16,11 +16,18 @@ handler.help = ['loli']
 export default handler
 
 global.ship = [
-"https://telegra.ph/file/9720e672d4013b367dd9f.jpg",
-"https://telegra.ph/file/a082dc8c30501c7d131ce.jpg",
-"https://telegra.ph/file/f0da437b40b3172dddc1e.jpg",
-"https://telegra.ph/file/8ce7701cafdd85afe0341.jpg",
-"https://telegra.ph/file/460d53dcfeb062ebc1f0a.jpg",
-"https://telegra.ph/file/ecd05c24a20e39fe4b342.jpg",
+"https://media.tenor.com/2hzy4e41AS0AAAAC/zhang-namra.gif",
+"https://media.tenor.com/8nt2OOrR9_gAAAAC/anime-girl.gif",
+"https://media.tenor.com/cMhJo-f2PDkAAAAC/anime-aesthetic-anime-kawaii.gif",
+"https://media.tenor.com/g9HjxRZM2C8AAAAd/anime-love.gif",
+"https://media.tenor.com/SRRnWa6nMAoAAAAC/foxplushy-foxy.gif",
+"https://media.tenor.com/eDROfBxIq5kAAAAC/teruko-teruko-ookura.gif",
+"https://media.tenor.com/GT2-EGuUogQAAAAC/anime-girl.gif",
+"https://media.tenor.com/4Oz_B30BYqEAAAAC/kaguya-sama-love-is-war-fujiwara-chika.gif",
+"https://media.tenor.com/8-F6-nB1L84AAAAC/anime-blush.gif",
+"https://media.tenor.com/jvkHnroaIWEAAAAC/heart-anime.gif",
+"https://media.tenor.com/2Kbfu1HwdPcAAAAC/anime-in-love-with-you.gif",
+"https://media.tenor.com/hJa9StBduMcAAAAd/makima-chainsaw-man.gif",
+"https://media.tenor.com/2Up_E7YqMoYAAAAC/helltaker-modeus.gif",
 
 ]
